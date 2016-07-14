1a5120e3e9aed2e61e20b15ff1905030bdaf6d8b
Paul Ryan <paul.rar@gmail.com>
2016-07-14 11:18:39 -1000

    Add script to change the publish metabox
    Only loads for users without the publish_{post_type} capability.

---
+++ js/modify-publish-metabox.js
@@ -0,0 +1,10 @@
+// If we're editing a post and the current user doesn't have the
+// publish_{post_type} capability, this script gets loaded that modifies the
+// publish metabox.
+( function ( $ ) {
+	$( document ).ready( function( $ ) {
+
+		$( '#publish' ).val( 'Submit for Review' );
+
+	});
+})( jQuery );
--- lib/class-lrp-controller.php
+++ lib/class-lrp-controller.php
@@ -11,6 +11,7 @@ class LRP_Controller {
 	 */
 	function __construct() {
 		add_action( 'plugins_loaded', array( $this, 'plugins_loaded__load_textdomain' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts__modify_publish_metabox' ), 10, 1 );
 		add_action( 'save_post', array( $this, 'save_post__revert_if_unprivileged' ), 1, 3 );
 
 	}
@@ -25,6 +26,37 @@ class LRP_Controller {
 			false,
 			plugin_basename( dirname( dirname( __FILE__ ) ) ) . '/languages'
 		);
+	}
+
+	/**
+	 * Add script to modify the publish metabox to allow users without publish
+	 * capabilities to submit a revision for approval.
+	 * Action hook: https://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	 */
+	function admin_enqueue_scripts__modify_publish_metabox( $hook ) {
+		global $post, $wp_post_types;
+
+		// If we're editing a post and the current user doesn't have the
+		// publish_{post_type} capability, load the javascript that modifies the
+		// publish metabox.
+		if (
+			$hook === 'post.php' &&
+			! current_user_can( $wp_post_types[$post->post_type]->cap->publish_posts )
+		) {
+			wp_enqueue_script(
+				'lrp-modify-publish-metabox',
+				plugins_url( '/js/modify-publish-metabox.js', dirname( __FILE__ ) ),
+				array(),
+				'20160714'
+			);
+			wp_localize_script(
+				'lrp-modify-publish-metabox',
+				'lrp_L10n',
+				array(
+					'update' => __( 'Update' ),
+				)
+			);
+		}
 	}
 
 	/**
