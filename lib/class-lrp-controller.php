de9137b0dcaee4055db920b7d87cc7fcad4e8d4e
Paul Ryan <paul.rar@gmail.com>
2016-07-14 10:31:42 -1000

    Refactor to plugin class; switch revert mechanism to wp_restore_post_revision
    wp_restore_post_revision addresses all postmeta, obviating the need for
    using the acf/update_value hook to revert ACF fields.

--- lib/class-lrp-controller.php
+++ lib/class-lrp-controller.php
@@ -1,1 +1,66 @@
 <?php
+
+class LRP_Controller {
+	public $textdomain = 'limit-revision-publishing';
+	public $reviewers = array();
+	private $done = false;
+
+	/**
+	 * Class constructor.
+	 * Register all plugin hooks.
+	 */
+	function __construct() {
+		add_action( 'plugins_loaded', array( $this, 'plugins_loaded__load_textdomain' ) );
+		add_action( 'save_post', array( $this, 'save_post__revert_if_unprivileged' ), 1, 3 );
+
+	}
+
+	/**
+	 * Load textdomain for internationalization.
+	 * Action hook: https://codex.wordpress.org/Plugin_API/Action_Reference/plugins_loaded
+	 */
+	function plugins_loaded__load_textdomain() {
+		load_plugin_textdomain(
+			$this->textdomain, // 'limit-revision-publishing'
+			false,
+			plugin_basename( dirname( dirname( __FILE__ ) ) ) . '/languages'
+		);
+	}
+
+	/**
+	 * Save this post as a revision and revert to the previous revision if the
+	 * current user doesn't have the publish_{post_type} capability.
+	 * Action hook: https://codex.wordpress.org/Plugin_API/Action_Reference/save_post
+	 * @param int $post_id The post ID.
+	 * @param post $post The post object.
+	 * @param bool $update Whether this is an existing post being updated or not.
+	 */
+	function save_post__revert_if_unprivileged( $post_id, $post, $update ) {
+		global $wp_post_types;
+
+		// Allow this save (don't revert) if we've already reverted, if this post
+		// isn't published yet, if this is an autosave, or if the current user has
+		// the publish_{post_type} capability.
+		if (
+			$this->done ||
+			$post->post_status !== 'publish' ||
+			( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) ||
+			current_user_can( $wp_post_types[$post->post_type]->cap->publish_posts )
+		) {
+			return;
+		}
+
+		// Revert to previous revision (latest revision is this one, so we want the
+		// one right before it).
+		$this->done = true;
+		$previous_revision = array_pop( wp_get_post_revisions( $post_id, array(
+			'posts_per_page' => 2,
+			'cache_results' => false,
+		) ) );
+		wp_restore_post_revision( $previous_revision );
+
+		// TODO: send notification emails to reviewers.
+
+	}
+
+}
--- plugin.php
+++ plugin.php
@@ -15,73 +15,16 @@
  */
 
 /**
- * LRP_Controller class.
+ * Include plugin classes.
  */
 if ( ! class_exists( 'LRP_Controller' ) ) {
 	require_once dirname( __FILE__ ) . '/lib/class-lrp-controller.php';
 }
 
-
-// If the current user doesn't have the publish_{post_type} capability and
-// we're saving ACF fields on the main post, revert the changes to the
-// existing value. Note: ACF field updates on the revision ID will go through.
-// A more privileged user will have to approve the revision to make the
-// changes apply to the main post ID.
-function revert_acf_update_if_unprivileged( $value, $post_id, $field ) {
-	global $wp_post_types;
-
-	$post_type = get_post_type( $post_id );
-	$publish_capability = $wp_post_types[$post_type]->cap->publish_posts;
-	if (
-		! current_user_can( $publish_capability ) &&
-		! wp_is_post_revision( $post_id ) &&
-		get_post_status( $post_id ) === 'publish'
-	) {
-		// Reset to the existing field value.
-		$value = get_field( $field['key'], $post_id );
-	} else if (	wp_is_post_revision( $post_id ) && isset( $GLOBALS['creating_unchanged_revision_after_changed_revision'] ) ) {
-		// We are creating the "unchanged" revision after the changed revision, so
-		// make sure it gets all the original, unchanged ACF field values.
-		$parent_post_id = wp_get_post_parent_id( $post_id );
-		$value = get_field( $field['key'], $parent_post_id );
-	}
-
-	return $value;
+// Instantiate plugin.
+if ( class_exists( 'LRP_Controller' ) ) {
+	$limit_revision_publishing = new LRP_Controller();
 }
-add_filter( 'acf/update_value', 'revert_acf_update_if_unprivileged', 10, 3 );
-
-
-// If the current user doesn't have the publish_{post_type} capability and
-// we're saving the main post (not a revision), revert the changes to the
-// existing value. Note: Updates to the revision ID will go through.
-// A more privileged user will have to approve the revision to make the
-// changes apply to the main post ID.
-function revert_update_if_unprivileged( $post_id, $post_after, $post_before ) {
-	global $wp_post_types;
-
-	$post_type = get_post_type( $post_id );
-	$publish_capability = $wp_post_types[$post_type]->cap->publish_posts;
-	if (
-		! current_user_can( $publish_capability ) &&
-		! wp_is_post_revision( $post_id ) &&
-		get_post_status( $post_id ) === 'publish'
-	) {
-		// Unhook update functions so they don't loop infinitely.
-		remove_action( 'post_updated', 'revert_update_if_unprivileged' );
-
-		// Make a new revision with the original post.
-		// Flag the creation of this revision so our ACF update handler knows to use
-		// the original, unchanged field value(s) for this revision.
-		$GLOBALS['creating_unchanged_revision_after_changed_revision'] = true;
-		wp_update_post( $post_before );
-		unset( $GLOBALS['creating_unchanged_revision_after_changed_revision'] );
-
-		// Rehook update functions.
-		add_action( 'post_updated', 'revert_update_if_unprivileged', 10, 3 );
-	}
-
-}
-add_action( 'post_updated', 'revert_update_if_unprivileged', 10, 3 );
 
 
 function lrp_debug() {
